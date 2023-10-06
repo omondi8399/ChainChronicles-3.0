cdec2e5f3578b93ddb9773a1931a400cfbd789ab
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-10-06 21:27:40 +0300

    updated Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       <div className={styles.logo}>Chain Chronicles</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/" className={styles.link}>Homepage</Link>
+        <Link href="/" className={styles.link}>Homepage</Link> 
         <Link href="/" className={styles.link}>Contact</Link>
         <Link href="/" className={styles.link}>About</Link>
         <AuthLinks />
