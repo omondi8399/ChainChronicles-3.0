b7ffd0849463bf2876fae7b7aa4cfc6c62234c16
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-10-06 21:28:54 +0300

    updated login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -19,11 +19,11 @@ const LoginPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton} onClick={() => signIn("google")}>
+        {/* <div className={styles.socialButton} onClick={() => signIn("google")}>
           Sign in with Google
-        </div>
-        <div className={styles.socialButton}>Sign in with Github</div>
-        <div className={styles.socialButton}>Sign in with Facebook</div>
+        </div> */}
+        <div className={styles.socialButton} onClick={() => signIn("github")}>Sign in with Github</div>
+        {/* <div className={styles.socialButton}>Sign in with Facebook</div> */}
       </div>
     </div>
   );
